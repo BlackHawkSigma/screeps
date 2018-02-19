b3938191771c437b8c870a17c20fd40a14d8ce99
Tobias Beese <tobias.beese@gmx.de>
2018-02-19 21:00:02 +0100

    Fix Typo

--- spawnRules.js
+++ spawnRules.js
@@ -6,7 +6,7 @@ const rules = {
   check (spawnName) {
     const harvesters = _.filter(Game.creeps, c => c.memory.role === 'harvester')
 
-    if (harvesters.lenght < minHarvesters) {
+    if (harvesters.length < minHarvesters) {
       Game.spawns[spawnName].spawnCreep(harvesterConfig, `Harvester${Game.time}`, {
         memory: {role: 'harvester'}
       })
@@ -15,13 +15,12 @@ const rules = {
     if (Game.spawns[spawnName].spawning) {
       let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name]
       Game.spawns[spawnName].room.visual.text(
-           '🛠️' + spawningCreep.memory.role,
-           Game.spawns[spawnName].pos.x + 1,
-           Game.spawns[spawnName].pos.y,
-           {align: 'left', opacity: 0.8})
-       }
+        '🛠️' + spawningCreep.memory.role,
+        Game.spawns[spawnName].pos.x + 1,
+        Game.spawns[spawnName].pos.y,
+        {align: 'left', opacity: 0.8})
+    }
   }
-
 }
 
 module.exports = rules
