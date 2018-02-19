de931b89551b1f453899f838ee629e7f0973031c
Tobias Beese <tobias.beese@gmx.de>
2018-02-19 21:24:56 +0100

    Extend auto spawning

--- spawnRules.js
+++ spawnRules.js
@@ -1,17 +1,29 @@
-const harvesterConfig = [WORK, CARRY, MOVE, MOVE]
-const minHarvesters = 4
-
+const simpleConfig = [WORK, CARRY, MOVE, MOVE]
+const minHarvesters = 2
+const minUpgraders = 3
+const minBuilders = 1
 
 const rules = {
   check (spawnName) {
     const harvesters = _.filter(Game.creeps, c => c.memory.role === 'harvester')
+    const upgraders = _.filter(Game.creeps, c => c.memory.role === 'upgrader')
+    const builders = _.filter(Game.creeps, c => c.memory.role === 'builder')
 
     if (harvesters.length < minHarvesters) {
-      Game.spawns[spawnName].spawnCreep(harvesterConfig, `Harvester${Game.time}`, {
+      Game.spawns[spawnName].spawnCreep(simpleConfig, `Harvester${Game.time}`, {
         memory: {role: 'harvester'}
+      })
+    } else if (upgraders.length < minUpgraders) {
+      Game.spawns[spawnName].spawnCreep(simpleConfig, `Upgrader${Game.time}`, {
+        memory: {role: 'upgrader'}
+      })
+    } else if (builders.length < minBuilders) {
+      Game.spawns[spawnName].spawnCreep(simpleConfig, `Builder${Game.time}`, {
+        memory: {role: 'builder'}
       })
     }
 
+    // Show info
     if (Game.spawns[spawnName].spawning) {
       let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name]
       Game.spawns[spawnName].room.visual.text(
