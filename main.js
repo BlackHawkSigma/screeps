f58426b36e9d618c74efe81a0fa7e4392c997b2b
Tobias Beese <tobias.beese@gmx.de>
2018-02-19 20:28:35 +0100

    Add worker spawning

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const roleHarvester = require('role.harvester')
 const roleBuilder = require('role.builder')
+const spawnRules = require('spawnRules')
 
 module.exports.loop = function () {
+
+  spawnRules.check('Spawn1')
+
   for (let name in Game.creeps) {
     let creep = Game.creeps[name]
     if (creep.memory.role === 'harvester') {
---
+++ spawnRules.js
@@ -0,0 +1,27 @@
+const harvesterConfig = [WORK, CARRY, MOVE, MOVE]
+const minHarvesters = 4
+
+
+const rules = {
+  check (spawnName) {
+    const harvesters = _.filer(Game.creeps, c => c.memory.role === 'harvester')
+
+    if (harvester.lenght < minHarvesters) {
+      Game.spawns[spawnName].spawnCreep(harvesterConfig, `Harvester${Game.time}`, {
+        memory: {role: 'harvester'}
+      })
+    }
+
+    if (Game.spawns[spawnName].spawning) {
+      let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name]
+      Game.spawns[spawnName].room.visual.text(
+           '🛠️' + spawningCreep.memory.role,
+           Game.spawns[spawnName].pos.x + 1,
+           Game.spawns[spawnName].pos.y,
+           {align: 'left', opacity: 0.8})
+       }
+  }
+
+}
+
+module.exports = rules
